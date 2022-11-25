89e7294a14bd421e350035168afec2b9c69e0126
codergalib20 <coderboygalif@gmail.com>
2022-11-26 02:47:04 +0600

    submit server url

--- src/feature/api/apiSlice.ts
+++ src/feature/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://drive-manager-server.onrender.com",
     prepareHeaders: (headers, { getState, endpoint }) => {
       const token = (getState() as any)?.auth?.token;
       if (token) {
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 export default function useAuth() {
   const auth = useSelector<any>((state) => state.auth);
-  if (!(auth as any)?.token && !(auth as any)?.user && !(auth as any)?.user.email) {
+  if (!(auth as any)?.token && !(auth as any)?.user && !(auth as any)?.user?.email) {
     return false;
   } else {
     return true;
